4ea7ffac096c2e10adbc74aeabc4c15df103f90a
Sparrow <149317656+Sparrow3699@users.noreply.github.com>
2023-12-28 19:25:19 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/privateShareFilesbot'>Click here</a>\n○ Channel : @privateShareFilesbot\n○ Support Group : @privateShareFilesbot</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/BASHAMOVIE'>Click here</a>\n○ Channel : @BASHAMOVIE\n○ Support Group : @BASHAMOVIE</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
