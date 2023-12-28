7db953deadd44caa0cfab61c46513b1cfcfaf7a0
Sparrow <149317656+Sparrow3699@users.noreply.github.com>
2023-12-28 20:28:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "5055470937"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://khadarbasha23:khadarbasha@cluster0.x60phmi.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://unzipbot:unzipbot@cluster0.nalwltl.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "bashamoviefiledb")
 
 #force sub channel id, if you want enable force sub
